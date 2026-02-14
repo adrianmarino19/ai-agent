4bc1c63e4a2ff3f09d824d8f2af973a207613f71
Adrian Marino <adrianmarino10@gmail.com>
2026-02-14 11:59:34 +0100

    setup: v1 of repo structure

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-def main():
-    print("Hello from ai-agent!")
+import os
+from dotenv import load_dotenv
+from google import genai
 
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+if api_key:
+    raise RuntimeError("Please put a valid API Key...")
+
+
+client = genai.Client(api_key=api_key)
 
 if __name__ == "__main__":
     main()
