960c66b94bc063d26cc59ef30757a0dfc1f00e5c
Adrian Marino <adrianmarino10@gmail.com>
2026-02-14 12:13:52 +0100

    setup: Gemini tryout

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 import os
+import sys
+
 from dotenv import load_dotenv
 from google import genai
+
+from utils import MODEL_ID
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
-if api_key:
-    raise RuntimeError("Please put a valid API Key...")
+if not api_key:
+    raise RuntimeError("Please input a valid API Key")
 
 
 client = genai.Client(api_key=api_key)
 
+
+response = client.models.generate_content(
+    model=MODEL_ID,
+    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+)
+
 if __name__ == "__main__":
-    main()
+    print(response.text)
