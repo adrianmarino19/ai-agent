868073ed6e4087778532b9d5d976ca59fe9cb89f
Adrian Marino <adrianmarino10@gmail.com>
2026-02-20 19:53:29 +0100

    fix: I can now call and get results

--- call_function.py
+++ call_function.py
@@ -1,7 +1,10 @@
-from functions.get_files_info import schema_get_files_info
-from functions.get_file_content import schema_get_file_content
-from functions.write_file import schema_write_file
-from functions.run_python_file import schema_run_python_file
+from typing import Any
+
+
+from functions.get_files_info import schema_get_files_info, get_files_info
+from functions.get_file_content import schema_get_file_content, get_file_content
+from functions.write_file import schema_write_file, write_file
+from functions.run_python_file import schema_run_python_file, run_python_file
 
 from google.genai import types
 
@@ -13,3 +16,64 @@ available_functions = types.Tool(
         schema_run_python_file,
     ],
 )
+
+FUNCTION_MAP = {
+    "get_file_content": get_file_content,
+    "write_file": write_file,
+    "get_files_info": get_files_info,
+    "run_python_file": run_python_file,
+}
+
+
+def call_function(
+    function_call: list[types.FunctionCall],
+    verbose: bool = False,
+) -> types.Content:
+    function_result = []
+
+    for function in function_call:
+        if function.name in FUNCTION_MAP:
+            function_name = function.name or ""
+            function_args = dict[str, Any](function.args) if function.args else {}
+            function_args["working_directory"] = "./calculator"
+
+            if not verbose:
+                print(f" - Calling function: {function.name}")
+            else:
+                print(f"Calling function: {function.name}({function.args})")
+
+            function_result.append(FUNCTION_MAP[function_name](**function_args))
+
+            return types.Content(
+                role="tool",
+                parts=[
+                    types.Part.from_function_response(
+                        name=function_name,
+                        response={"result": function_result},
+                    )
+                ],
+            )
+
+        # So this can only call one function....
+
+        else:
+            return types.Content(
+                role="tool",
+                parts=[
+                    types.Part.from_function_response(
+                        name=function_name,
+                        response={"error": f"Unknown function: {function_name}"},
+                    )
+                ],
+            )
+
+
+# return types.Content(
+#     role="tool",
+#     parts=[
+#         types.Part.from_function_response(
+#             name=function_name,
+#             response={"error": f"Unknown function: {function_name}"},
+#         )
+#     ],
+# )
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from google import genai
 from google.genai import types
 
 from prompts import system_prompt
-from call_function import available_functions
+from call_function import available_functions, call_function
 from config import MODEL_ID, client
 from cli import get_query_cli
 
@@ -19,9 +19,7 @@ def get_metadata(response: types.GenerateContentResponse):
     return meta_data
 
 
-def generate_response(
-    query: str, verbose: bool | None = None
-) -> types.GenerateContentResponse:
+def generate_response(query: str, verbose: bool | None = None) -> str | None:
     messages = [types.Content(role="user", parts=[types.Part(text=query)])]
     response = client.models.generate_content(
         model=MODEL_ID,
@@ -33,7 +31,7 @@ def generate_response(
 
     if verbose:
         meta_data = get_metadata(response)
-        print(f"User prompt: {user_query}")
+        print(f"User prompt: {query}")
         print(f"Prompt tokens: {meta_data.prompt_token_count}")
         print(f"Response tokens: {meta_data.candidates_token_count}")
 
@@ -41,10 +39,31 @@ def generate_response(
         return f"Response: {response.text}"
     else:
         function_call = response.function_calls
-        for function in function_call:
-            print(f"Calling function: {function.name}({function.args})")
+        function_call_result = call_function(function_call)
+
+        final_result = []
+        if (
+            not function_call_result.parts
+            or function_call_result.parts[0].function_response is None
+            or function_call_result.parts[0].function_response.response is None
+        ):
+            raise ValueError("Function call result has no parts")
+
+        final_result.append(function_call_result.parts[0])
+
+        if verbose:
+            print(f"-> {function_call_result.parts[0].function_response.response}")
+            return
 
 
 if __name__ == "__main__":
-    user_query, verbose = get_query_cli()
-    response = generate_response(user_query)
+    query, verbose = get_query_cli()
+    response = generate_response(query, verbose)
+
+
+# The types.Content object that we return from call_function should have a non-empty .parts list. If it doesn't, raise an exception.
+# We want to look at the .function_response property of the first item in the list of parts, i.e. .parts[0].function_response. It should be a FunctionResponse object. If it's somehow None, raise an exception.
+# Finally, we need to check the .response field of the FunctionResponse object, i.e. .parts[0].function_response.response. This is where the actual function result will be. If it's None, raise an exception.
+# Whew! Now that we know that some response came back from the function call, add .parts[0] to a list of function results. We'll use that later.
+# If verbose mode is in effect, print the result of the function call like this:
+# print(f"-> {function_call_result.parts[0].function_response.response}")
